ee335446f8fe2ed5ff7042808be7e0f7aa017e75
Colin Broderick <rusty@therustyproject.com>
2012-01-06 00:23:45 +0000

    updated logos and root url

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ MetreMap::Application.routes.draw do
   resources :protected_structures
 
   resources :meters
-
+  root :to => 'protected_structures#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +52,7 @@ MetreMap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'meters#index'
+   
 
   # See how all your routes lay out with "rake routes"
 
